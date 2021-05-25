4bbeac8bd3ea05d1c87fef94046a70a24030cc10
girijapj03 <72901278+girijapj03@users.noreply.github.com>
2021-05-25 14:10:51 +0530

    Update primdaigomatrix.java

--- primdaigomatrix.java
+++ primdaigomatrix.java
@@ -1,10 +1,10 @@
-class Secdaigomatrix{
+class Primdaigomatrix{
 public static void main(String []a){
 System.out.println("started main()");
 int row=6;
 for(int i=1; i<=row; i++){
 for(int j=1; j<=row; j++){
-if(i==j || i+j==row+1)
+if(i==j )
 System.out.print(i+""+j +" ");
 else
 System.out.print("   ");
